dc734a2697c13f54c85b2e9eeb19bbaf18163df0
reynaldo29 <acm19@outlook.com>
2022-11-10 11:02:50 -0500

    123

--- database.js
+++ database.js
@@ -1,6 +1,4 @@
 const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGODB_URI,{
-    useNewUrlParser: true
-})
+mongoose.connect(process.env.MONGODB_URI)
     .then(db => console.log('DB is connected'))
     .catch(err => console.error(err));
