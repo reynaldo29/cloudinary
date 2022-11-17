91b915941ebb0ac9456dfab1d95796436e11946c
reynaldo29 <acm19@outlook.com>
2022-11-17 09:11:14 -0500

    ff

--- database.js
+++ database.js
@@ -1,4 +1,4 @@
 const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGODB_URI)
+mongoose.connect("mongodb+srv://adminadmin:sinergismo12@cluster0.1ac1k55.mongodb.net/test?retryWrites=true&w=majority")
     .then(db => console.log('DB is connected'))
     .catch(err => console.error(err));
